6ce7b763f89128ff5e40eeec5898be136f3bfa25
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-15 20:19:53 +0300

    registration, login, logout

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/guzzle', function () {
     
     dd(json_decode($response->getBody()->getContents()));
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
